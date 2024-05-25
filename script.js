95ab64363de6bb98983dd40ec5fd69a04286a5cc
alemendozarojas <alemendozarojas@gmail.com>
2024-05-25 03:37:15 -0300

    registro

--- script.js
+++ script.js
@@ -5,19 +5,11 @@ function validarFormulario() {
     var interes = document.getElementById("interes").value;
     var sexo = document.querySelector('input[name="sexo"]:checked');
     var terminos = document.getElementById("terminos").checked;
-    const open=document.getElementById('open');
-    const modal_container=document.getElementById('modal_container');
-    const close=document.getElementById('close');   
     
-    if (nombre === "" || email === "" || telefono === "" || interes === "" || !sexo || !terminos) {}
-       return false;
-    else {
-        open.addEventListener('click', ()=>{
-            modal_container.classList.add('show');
-        });
-        close.addEventListener('click', () =>{
-            modal_container.classList.remove('show');
-        });
-        return true;
+    if (nombre === "" || email === "" || telefono === "" || interes === "" || !sexo || !terminos) {
+        alert("Por favor completa todos los campos obligatorios.");
+        return false;
     }
+    
+    return true;
 }
